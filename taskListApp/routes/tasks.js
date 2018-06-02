57a41d2da8c10b9b8df98c6fa69f003e66e1bc6c
Adib Ahmad <asalhani@outlook.com>
2018-06-02 08:47:22 +0300

    configure mongodb on cloud (mLab service)

--- taskListApp/routes/tasks.js
+++ taskListApp/routes/tasks.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+
+var db = mongojs('mongodb://admin:asw123456@ds245680.mlab.com:45680/mytasklistapp', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send("Tasks api worked");
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    })
 });
 
 module.exports = router;
