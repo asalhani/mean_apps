8cc976f9c5741fcbe48aacdf750c3a47858ed30b
Adib Ahmad <asalhani@outlook.com>
2018-06-07 01:08:49 +0300

    make basic get request

--- nodeCourseSamples/02-WeatherApp/app.js
+++ nodeCourseSamples/02-WeatherApp/app.js
@@ -4,5 +4,8 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3668%20Uthman%20Al%20Harqumi,%20As%20Sulimaniyah',
     json: true
 },(error, response, body) =>{
-    console.log(JSON.stringify(body, undefined,3));
+     //console.log(JSON.stringify(body, undefined,3));
+    //console.log(JSON.stringify(response, undefined, 3));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Location: lat: ${body.results[0].geometry.location.lat}, lng: ${body.results[0].geometry.location.lng}`)
 });
