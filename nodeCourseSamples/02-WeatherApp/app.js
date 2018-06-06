5a06681600e6068ab1c92e403915acc0410a2f1d
Adib Ahmad <asalhani@outlook.com>
2018-06-07 01:29:36 +0300

    handle http request error

--- nodeCourseSamples/02-WeatherApp/app.js
+++ nodeCourseSamples/02-WeatherApp/app.js
@@ -1,11 +1,36 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch wather for',
+        string: true
+    }
+})
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+    console.log(argv);
+
+    var encodedAddress = encodeURIComponent(argv.address);
+    //console.log(encodedAddress);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=3668%20Uthman%20Al%20Harqumi,%20As%20Sulimaniyah',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
-},(error, response, body) =>{
-     //console.log(JSON.stringify(body, undefined,3));
+}, (error, response, body) => {
+    if(error){
+        console.log("ERROR ==> Google Error", error);
+    } else if(body.status === 'ZERO_RESULTS'){
+        console.log('ERROR ==> Unable to find address');
+    } else if(body.status === 'OK') {
+         //console.log(JSON.stringify(body, undefined,3));
     //console.log(JSON.stringify(response, undefined, 3));
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`Location: lat: ${body.results[0].geometry.location.lat}, lng: ${body.results[0].geometry.location.lng}`)
+    }
+   
 });
