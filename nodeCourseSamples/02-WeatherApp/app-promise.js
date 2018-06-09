d18fbd176dd1a05274c4a0b5cf62aa46e1d9c132
Adib Ahmad <asalhani@outlook.com>
2018-06-10 02:52:34 +0300

    refactor code to use axios library

--- nodeCourseSamples/02-WeatherApp/app-promise.js
+++ nodeCourseSamples/02-WeatherApp/app-promise.js
@@ -3,6 +3,7 @@
 
 
 const yargs = require('yargs');
+const axios = require('axios');
 const geocode = require('./geocode/geocode.js');
 const forecast = require('./geocode/forecast');
 
@@ -20,14 +21,26 @@ const argv = yargs.options({
 
 // console.log(argv);
 
-// using promisses
-geocode.getGeocodeAddressAsync(argv.address)
-    .then((result => {
-        console.log(result);
-        debugger
-        return forecast.getTempretureAsync(result.lat, result.lng);
-    })).then((result) =>{
-        console.log(result);
-    }).catch((errorMessage) => {
-        console.log(errorMessage);
-    });
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+// make axos request
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find address..');
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/24b60d5f2a4b9edb6698a8a52522fce7/${lat},${lng}`;
+    return axios.get(weatherUrl);
+}).then((weatherResponse) =>{
+    console.log(weatherResponse.data.currently.temperature);
+}).catch((e) => {
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to Google maps API..');
+    }
+    else {
+        console.log(e.message);
+    }
+
+});
