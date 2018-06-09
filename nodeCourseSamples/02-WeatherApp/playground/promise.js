4cd0a6969f72e6ec0d545761f0996d51cf770a50
Adib Ahmad <asalhani@outlook.com>
2018-06-09 18:06:37 +0300

    pass paramters to promise

--- nodeCourseSamples/02-WeatherApp/playground/promise.js
+++ nodeCourseSamples/02-WeatherApp/playground/promise.js
@@ -1,12 +1,30 @@
-var somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve("it worked...");
-        reject("unable to fulfill promise");
-    }, 2500);
-});
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            }
+            else {
+                reject("argus. should be numbers")
+            }
+        }, 1500);
+    });
+};
 
-somePromise.then((message) => {
-    console.log("succ. ", message);
-}, (errorMessage) =>{
-    console.log("error ==> ", errorMessage);
+asyncAdd(1, 2).then((result) => {
+    console.log("success: ", result);
+}, (error) => {
+    console.log("ERR ==> ", error);
 });
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve("it worked...");
+//         reject("unable to fulfill promise");
+//     }, 2500);
+// });
+
+// somePromise.then((message) => {
+//     console.log("succ. ", message);
+// }, (errorMessage) =>{
+//     console.log("error ==> ", errorMessage);
+// });
